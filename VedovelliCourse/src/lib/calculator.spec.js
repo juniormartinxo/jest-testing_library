f9f260ba1a0c6d474381d4901228a34b69f43752
Junior Martins <amjr.box@gmail.com>
2021-11-27 17:47:21 -0300

    TDD Calculator

---
+++ VedovelliCourse/src/lib/calculator.spec.js
@@ -0,0 +1,27 @@
+const { sum } = require('./calculator')
+
+it('should sum 2 and 2 and result must be 4', () => {
+  expect(sum(2, 2)).toBe(4)
+})
+
+it('should sum 2 and 2 even if one of them is a string and the result must be for', () => {
+  expect(sum('2', '2')).toBe(4)
+})
+
+it('should throw an error if what provided to the method cannot be summed', () => {
+  expect(() => {
+    sum('', '2')
+  }).toThrowError()
+
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError()
+
+  expect(() => {
+    sum({})
+  }).toThrowError()
+
+  expect(() => {
+    sum()
+  }).toThrowError()
+})
